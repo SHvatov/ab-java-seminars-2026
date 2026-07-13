8a9780207865b6f692d07a458d5182207f169136
Sergey Hvatov <ishvatov@gmail.com>
2026-07-13 17:50:15 +0300

    init: parent Maven pom + basic/advanced modules skeleton

---
+++ advanced/src/main/java/academy/backend/advanced/Main.java
@@ -0,0 +1,7 @@
+package academy.backend.advanced;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ basic/src/main/java/academy/backend/basic/Main.java
@@ -0,0 +1,7 @@
+package academy.backend.basic;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
